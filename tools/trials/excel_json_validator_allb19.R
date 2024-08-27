150c083f2984e03376c4f792a177fc1f3ee7ddf7
MarikoOhtsuka <mariko.ohtsuka@nnh.go.jp>
2024-08-27 13:17:41 +0900

    temp

---
+++ tools/trials/excel_json_validator_allb19.R
@@ -0,0 +1,80 @@
+#' title
+#' description
+#' @file excel_json_validator_tran.R
+#' @author Mariko Ohtsuka
+#' @date 2024.8.27
+keep_objects <- c("jsonList", "sheetList")
+rm(list = setdiff(ls(), keep_objects))
+# ------ libraries ------
+library(tidyverse)
+library(here)
+source(here("tools", "excel_json_validator_common.R"), encoding="UTF-8")
+# ------ constants ------
+# ------ functions ------
+# ------ main ------
+fieldItems <- jsonList |> GetFieldItemsByJsonList()
+jpNameAndAliasName <- jsonList |>  GetNameAndAliasNameByJson()
+checkChecklist <- list()
+##############
+# item sheet #
+##############
+jsonSheetItemList <- GetItemFromJson(sheetList, jsonList)
+df_item_json <- jsonSheetItemList$json |> as.data.frame() %>% mutate(across(everything(), ~ ifelse(is.na(.), "", .)))
+df_item_sheet <- jsonSheetItemList$sheet |> as.data.frame() %>% mutate(across(everything(), ~ ifelse(is.na(.), "", .)))
+checkChecklist$item <- CheckTarget(df_item_sheet, df_item_json)
+##############
+# allocation #
+##############
+checkChecklist$allocation <- sheetList |> CheckAllocation(jsonList)
+##########
+# action #
+##########
+checkChecklist$action <- sheetList |> CheckAction()
+###########
+# display #
+###########
+checkChecklist$display <- sheetList |> CheckDisplay()
+##########
+# number #
+##########
+checkChecklist$number <- sheetList |> CheckNumber()
+########
+# name #
+########
+checkChecklist$name <- sheetList |> CheckName(jsonList)
+##########
+# option #
+##########
+checkChecklist$option <- sheetList |> CheckOption()
+###########
+# comment #
+###########
+checkChecklist$content <- sheetList |> CheckContent()
+###############
+# explanation #
+###############
+checkChecklist$explanation <- sheetList |> CheckExplanation()
+############
+# presence #
+############
+checkChecklist$presence <- sheetList |> CheckPresence()
+##########
+# master #
+##########
+checkChecklist$master <- sheetList |> CheckMaster()
+#########
+# visit #
+#########
+checkChecklist$visit <- sheetList |> CheckVisit()
+#########
+# alert #
+#########
+checkChecklist$alert <- sheetList |> CheckAlert()
+#########
+# title #
+#########
+checkChecklist$title <- sheetList |> CheckTitle()
+############
+# assigned #
+############
+checkChecklist$assigned <- sheetList |> CheckAssigned()
