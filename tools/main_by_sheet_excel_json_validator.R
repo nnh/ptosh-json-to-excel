0395eb1699f13acfe35bf44b734f8f9c94ec9af0
MarikoOhtsuka <mariko.ohtsuka@nnh.go.jp>
2024-09-05 08:49:49 +0900

    issue #34

--- tools/main_by_sheet_excel_json_validator.R
+++ tools/main_by_sheet_excel_json_validator.R
@@ -9,15 +9,35 @@ library(here)
 source(here("tools", "by_sheet_excel_json_validator.R"), encoding="UTF-8")
 # ------ constants ------
 ignoreCheckFlag <- list(
-  Field_Items=T,
-  Option=T,
-  Flip_Flops=T,
+  Field_Items=F,
+  Option=F,
+  Flip_Flops=F,
   Cdisc_Sheet_Configs=T,
   Cdisc_Sheet_Configs_Pivot=F,
-  Allocation=T
+  Allocation=F
 )
 kTargetTrials <- c("tran", "gpower", "allb19", "allr23", "bev")
 # ------ main ------
 for (i in 1:length(kTargetTrials)) {
   kTargetTrials[[i]] |> ExecCompareBySheet()
+}
+
+for (i in 1:length(kTargetTrials)) {
+  temp <- get(str_c("check_", kTargetTrials[i])) |> map( ~ {
+    temp <- . |> map( ~ {
+      if (!is.list(.)) {
+        return(NULL)
+      }
+      return(.)
+    }) |> discard( ~ is.null(.))
+    if (length(temp) == 0) {
+      return(NULL)
+    }
+    return(temp)
+  }) |> discard( ~ is.null(.))
+  if (length(temp) == 0) {
+    print(str_c("test ok:", kTargetTrials[i]))
+  } else {
+    stop(str_c("test ng:", kTargetTrials[i]))
+  }
 }
