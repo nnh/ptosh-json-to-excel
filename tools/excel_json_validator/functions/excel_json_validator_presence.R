abb1e3224792b238b3b261c65066d80ae9422b71
MarikoOhtsuka <mariko.ohtsuka@nnh.go.jp>
2025-05-16 09:01:48 +0900

    issue #51

--- tools/excel_json_validator/functions/excel_json_validator_presence.R
+++ tools/excel_json_validator/functions/excel_json_validator_presence.R
@@ -2,7 +2,7 @@
 #'
 #' @file excel_json_validator_presence.R
 #' @author Mariko Ohtsuka
-#' @date 2025.5.15
+#' @date 2025.5.16
 CheckPresence <- function(sheetList) {
     sheetName <- "presence"
     sheet <- sheetList[[sheetName]] |>
@@ -10,15 +10,89 @@ CheckPresence <- function(sheetList) {
     json <- GetPresenceFromJson()
     return(CheckTarget(sheet, json))
 }
+
 GetPresenceFromJson <- function() {
-    df <- map2(fieldItems, names(fieldItems), ~ {
-        fieldItem <- .x
-        aliasName <- .y
-        res <- fieldItem |> keep(~ .$type == "FieldItem::Article" & is.null(.$validators$presence))
-        presence <- res |> map_df(~ list(name = .$name, label = .$label))
-        presence$alias_name <- aliasName
-        return(presence)
-    }) |> bind_rows()
-    res <- GetItemsSelectColnames(df, c("jpname", "alias_name", "name", "label"))
+    jpnameAndAliasnameAndSheetId <- jsonList %>% map_df(~ list(sheet_id = .x$id, jpname = .x$name, alias_name = .x$alias_name))
+    articles <- fieldItems %>%
+        map(~ keep(.x, ~ .x$type == "FieldItem::Article"))
+    no_validators_presence <- articles %>%
+        list_c() %>%
+        keep(~ is.null(.x$validators$presence))
+    df_no_validators_presence <- no_validators_presence %>%
+        map_df(~ {
+            sheet_id <- .x$sheet_id
+            name <- .x$name
+            label <- .x$label
+            return(data.frame(sheet_id = sheet_id, name = name, label = label))
+        })
+    df_no_validators_presence_jpname_aliasname <- df_no_validators_presence %>%
+        inner_join(jpnameAndAliasnameAndSheetId, by = c("sheet_id" = "sheet_id")) %>%
+        arrange(alias_name, sheet_id)
+    cdiscSheetConfigs <- jsonList %>%
+        map(~ .x$cdisc_sheet_config)
+    excludeTargetsStat <- cdiscSheetConfigs %>%
+        list_c() %>%
+        map_df(~ {
+            prefix <- .x$prefix
+            sheet_id <- .x$sheet_id
+            table <- .x$table
+            fields <- NULL
+            for (i in seq_along(table)) {
+                if (is.null(table[[i]])) {
+                    next
+                }
+                if (table[[i]] != "STAT") {
+                    next
+                }
+                fields <- list(name = names(table)[[i]], value = table[[i]])
+            }
+            res <- fields %>% map_df(~.x)
+            res$prefix <- prefix
+            res$sheet_id <- sheet_id
+            return(res)
+        })
+
+    excludeTargetsIe <- cdiscSheetConfigs %>%
+        map(~ keep(.x, ~ .x$prefix == "IE")) %>%
+        keep(~ length(.x) > 0) %>%
+        list_c()
+    excludeTargetsIeorres <- excludeTargetsIe %>%
+        map_df(~ {
+            prefix <- .x$prefix
+            sheet_id <- .x$sheet_id
+            table <- .x$table
+            fields <- NULL
+            for (i in seq_along(table)) {
+                if (is.null(table[[i]])) {
+                    next
+                }
+                if (table[[i]] != "ORRES") {
+                    next
+                }
+                fields <- list(name = names(table)[[i]], value = table[[i]])
+            }
+            res <- fields %>% map_df(~.x)
+            res$prefix <- prefix
+            res$sheet_id <- sheet_id
+            return(res)
+        })
+    excludeTargets <- excludeTargetsIeorres %>%
+        bind_rows(excludeTargetsStat) %>%
+        inner_join(jpnameAndAliasnameAndSheetId, by = c("sheet_id" = "sheet_id")) %>%
+        arrange(alias_name, sheet_id)
+    presenceExcludeTargets <- df_no_validators_presence_jpname_aliasname %>%
+        anti_join(excludeTargets, by = c("sheet_id" = "sheet_id", "name" = "name")) %>%
+        select(c("jpname", "alias_name", "name", "label"))
+    if (nrow(presenceExcludeTargets) == 0) {
+        res <- data.frame(
+            jpname = "",
+            alias_name = "",
+            name = "",
+            label = "",
+            stringsAsFactors = FALSE
+        )
+    } else {
+        res <- presenceExcludeTargets
+    }
     return(res)
 }
