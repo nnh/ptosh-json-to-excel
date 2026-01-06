4cf96bc1ce65fa6eb62f6d3fd858b19da5bda2e5
MarikoOhtsuka <mariko.ohtsuka@nnh.go.jp>
2026-01-06 17:00:14 +0900

    issue #88

--- prg/functions/edit_item_visit.R
+++ prg/functions/edit_item_visit.R
@@ -1,4 +1,4 @@
-#' edit_item.R
+#' edit_item_visit.R
 #'
 #' @file edit_item.R
 #' @author Mariko Ohtsuka
@@ -7,6 +7,7 @@ EditItemVisit <- function(item_visit) {
     if (nrow(item_visit) == 0) {
         return(item_visit)
     }
+    # シートソート順の取得
     sheet_name_and_sort_order <- sheet_info %>%
         select(alias_name, sort_order) %>%
         distinct()
@@ -22,32 +23,40 @@ EditItemVisit <- function(item_visit) {
         slice_head(n = 1) %>%
         ungroup() %>%
         arrange(sort_order)
+    # 出力列順の取得
+    target_colnames_visits <- visit_info_for_item_visit %>%
+        pull(visit_group_name) %>%
+        as.vector()
+    output_colname_order <- target_colnames_visits %>%
+        c("label", ., "数値チェック・アラート条件の有無", "seq")
+    # visit_group毎にレコードをまとめる
+    target_item_visit <- item_visit %>%
+        inner_join(
+            visit_info_for_item_visit,
+            by = c("シート名英数字別名" = "alias_name")
+        ) %>%
+        select(visit_group_name, ラベル, `数値チェック・アラート条件の有無`)
+    target_item_visit_distinct <- target_item_visit %>%
+        distinct(.keep_all = TRUE)
+    # 出力行順の取得
     # 同一のラベルが複数存在する場合最も若い項目のみを有効として残す
-    field_label_and_sort_order <- field_list %>%
-        select(alias_name, label, field_seq)
-    sheet_and_field_sort_order <- sheet_name_and_sort_order %>%
-        left_join(field_label_and_sort_order, by = c("alias_name" = "alias_name")) %>%
-        arrange(sort_order, field_seq)
-    sheet_and_field_sort_order_len <- seq(length(sheet_and_field_sort_order$alias_name), 1)
-    kLowValue <- -999
-    for (i in sheet_and_field_sort_order_len) {
-        target_label <- sheet_and_field_sort_order$label[i]
-        temp <- sheet_and_field_sort_order %>%
-            filter(label == target_label & field_seq > kLowValue)
-        if (nrow(temp) > 1) {
-            sheet_and_field_sort_order[i, "field_seq"] <- kLowValue
-        }
-    }
-    # item_visit のラベル順に並び替え用のデータフレームを作成
-    item_visit_rownames <- sheet_and_field_sort_order %>%
-        filter(field_seq != kLowValue) %>%
+    target_field_items <- visit_info_for_item_visit %>%
+        inner_join(
+            field_list,
+            by = c("alias_name" = "alias_name")
+        ) %>%
+        select(visit_group_name, label, sort_order, field_seq) %>%
+        inner_join(
+            target_item_visit_distinct,
+            by = c("visit_group_name" = "visit_group_name", "label" = "ラベル")
+        )
+    item_visit_rownames <- target_field_items %>%
+        arrange(sort_order, field_seq) %>%
+        distinct(label, .keep_all = TRUE) %>%
         select("label")
     item_visit_rownames$seq <- seq(1, nrow(item_visit_rownames))
-    # item_visit を visit_group ごとに集約する
-    item_visit_by_group <- item_visit %>%
-        inner_join(visit_info_for_item_visit, by = c("シート名英数字別名" = "alias_name")) %>%
-        select(visit_group_name, ラベル, `数値チェック・アラート条件の有無`)
-    label_count_by_sheet <- item_visit_by_group %>%
+    # ラベル個数のカウント
+    label_count_by_sheet <- target_item_visit %>%
         group_by(visit_group_name, ラベル, `数値チェック・アラート条件の有無`) %>%
         summarise(
             ラベルの個数 = n(),
@@ -60,12 +69,6 @@ EditItemVisit <- function(item_visit) {
             values_from = ラベルの個数,
             values_fill = list(ラベルの個数 = 0)
         )
-    # 列順の生成
-    output_colname_order <- visit_info_for_item_visit %>%
-        pull(visit_group_name) %>%
-        as.vector() %>%
-        c("label", ., "数値チェック・アラート条件の有無", "seq")
-
     output_item_visit <- item_visit_rownames %>%
         inner_join(
             label_count_wide,
